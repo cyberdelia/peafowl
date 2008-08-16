6b11fe1c74613d52370df1058ee8c18bfbedd8e4
timothee.peignier <timothee.peignier@54597ca8-0954-0410-a2f0-27c18351348c>
2008-08-16 09:41:17 +0000

    update setup metadata
    git-svn-id: https://peafowl.googlecode.com/svn/trunk@16 54597ca8-0954-0410-a2f0-27c18351348c

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup
-#from distutils.core import setup
 
 setup(name='peafowl',
       version='0.3',
       description='A light weight server for reliable distributed message passing.',
+      long_description="Peafowl is a powerful but simple messaging server that enables reliable distributed queuing with an absolutely minimal overhead. It speaks the MemCache protocol for maximum cross-platform compatibility. Any language that speaks MemCache can take advantage of Peafowl's queue facilities.",
       author='Timothee Peignier',
       author_email='tim@tryphon.org',
-      url='http://pypi.python.org/pypi/peafowl/',
+      url='http://code.google.com/p/peafowl/',
+      license = 'MIT License',
+      platforms = ["Unix",],
+      keywords = "peafowl queue messaging distributed memcache starling",
+      classifiers = [ "Development Status :: 4 - Beta",
+                      "License :: OSI Approved :: MIT License",
+                      "Operating System :: Unix",
+                      "Programming Language :: Python" ],
       packages = ['peafowl'],
       scripts=['bin/peafowl'],
       test_suite='tests'
